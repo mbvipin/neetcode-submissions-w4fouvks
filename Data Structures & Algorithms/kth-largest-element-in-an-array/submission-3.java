316559d66e89acdf603ce51b8ebe19391c219732
Vipin Bhaskaran <mbvipin@gmail.com>
2026-05-18 06:58:14 +0100

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+        Queue<Integer> heap = new PriorityQueue<>();
+
+        for(int num: nums)
+        {
+            heap.offer(num);
+            if( heap.size() > k)
+            {
+                heap.poll();
+            }
+        }
+
+       return heap.peek();
+        
+    }
+}
