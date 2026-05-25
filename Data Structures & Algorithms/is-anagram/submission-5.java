081265715336aa858422d92ae204bd92dc044c5b
Vipin Bhaskaran <mbvipin@gmail.com>
2026-05-25 05:46:04 +0100

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if( s.length() != t.length())
+        {
+            return false;
+        }
+
+        int [] check = new int[26];
+
+        for(int i=0 ; i < s.length(); i++)
+        {
+            check[ s.charAt(i)-'a']++;
+            check[ t.charAt(i)- 'a']--;
+        }
+
+        for( int count: check)
+        {
+            if( count !=0 )
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
