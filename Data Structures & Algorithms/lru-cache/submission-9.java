773adb8e3e3264b170fbd5a35403e27e423c758a
Vipin Bhaskaran <mbvipin@gmail.com>
2026-05-31 11:07:22 +0100

    Add: lru-cache - submission-9

---
+++ Data Structures & Algorithms/lru-cache/submission-9.java
@@ -0,0 +1,32 @@
+class LRUCache {
+
+    private int capacity;
+    private Map<Integer,Integer> cache;
+
+    public LRUCache(int capacity) {
+
+        this.capacity= capacity;
+        this.cache= new LinkedHashMap<>(capacity, 0.75f,true)
+        {
+            @Override
+            public boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest)
+            {
+                return size() > LRUCache.this.capacity;
+            }
+
+        };
+        
+    }
+    
+    public int get(int key) {
+
+        return cache.getOrDefault(key,-1);
+        
+    }
+    
+    public void put(int key, int value) {
+
+        cache.put(key,value);
+        
+    }
+}
