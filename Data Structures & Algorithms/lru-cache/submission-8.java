5d3c1fb86a112c78664d1433ff23929a45fbb969
Vipin Bhaskaran <mbvipin@gmail.com>
2026-05-31 11:02:24 +0100

    Add: lru-cache - submission-8

---
+++ Data Structures & Algorithms/lru-cache/submission-8.java
@@ -0,0 +1,93 @@
+class Node
+{
+    private int key;
+    private int value;
+    private Node next ;
+    private Node prev;
+
+    public Node (int key, int value)
+    {
+        this.key= key;
+        this.value= value;
+    }
+
+
+}
+
+class LRUCache {
+
+    private int capacity;
+    private Map<Integer,Node> cache;
+    private Node left;
+    private Node right;
+
+    public LRUCache(int capacity) {
+
+        this.capacity= capacity;
+        this.cache= new HashMap<>();
+        this.left= new Node(0,0);
+        this.right= new Node(0,0);
+
+        this.left.next= this.right;
+        this.right.prev= this.left;
+        
+    }
+
+    private void remove(Node node)
+    {
+        Node prev= node.prev;
+        Node next= node.next;
+
+        prev.next= next;
+        next.prev= prev;
+    }
+
+    private void insert(Node node)
+    {
+        Node currentFirst= this.left.next;
+
+        this.left.next= node;
+        node.prev= this.left;
+        node.next= currentFirst;
+        currentFirst.prev= node;
+    }
+    
+    public int get(int key) {
+
+        if( cache.containsKey(key))
+        {
+            Node node= cache.get(key);
+            remove(node);
+            insert(node);
+    
+            return node.value;
+        }
+
+        return -1;
+        
+    }
+    
+    public void put(int key, int value) {
+
+        if( cache.containsKey(key))
+        {
+            Node node= cache.get(key);
+            remove(node);
+            node.value= value;
+            insert(node);
+            return;
+        }
+
+        if( cache.size() == capacity)
+        {
+            Node lru= this.right.prev;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+
+        Node newNode= new Node(key,value);
+        insert(newNode);
+        cache.put(key,newNode);
+        
+    }
+}
