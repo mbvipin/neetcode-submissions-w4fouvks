32cad6c450443a85c298820640caeae876d50776
Vipin Bhaskaran <mbvipin@gmail.com>
2026-06-10 07:50:12 +0100

    Add: shortest-path-in-binary-matrix - submission-0

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-0.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    private Integer[] getLoc(int row, int col)
+    {        return new Integer[] { row,col};
+    }
+
+    private String getPoint(int row, int col)
+    {        return row + "," + col;
+    }
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int ROWS= grid.length;
+        int COLS= grid[0].length;
+        if (grid[0][0] == 1 || grid[ROWS-1][COLS-1] == 1) return -1;
+
+        Queue<Integer[]> toProcess = new LinkedList<>();
+        Set<String> visited= new HashSet<>();
+        int [] [] directions = new int [][]
+        {
+            {1,0}, {-1,0}, {0,1}, {0,-1},
+            {1,1}, {1,-1}, {-1,1}, {-1,-1}
+        };
+
+        toProcess.offer(getLoc(0,0));
+        visited.add(getPoint(0,0));
+        int length= 1;
+        
+        while( !toProcess.isEmpty())
+        {
+            int size = toProcess.size();
+            for (int i = 0; i < size; i++) {
+                Integer [] polled= toProcess.poll();
+
+                if( polled[0] == ROWS -1 && polled[1] == COLS -1)
+                {
+                    return length;
+                }
+
+                for(int [] dir: directions)
+                {
+                    int newRow= polled[0] + dir[0];
+                    int newCol= polled[1]+ dir[1];
+
+                    if( newRow >= 0 && newCol >= 0 &&
+                        newRow < ROWS && newCol < COLS &&
+                        grid[newRow][newCol] == 0 &&
+                        ! visited.contains(getPoint(newRow,newCol)))
+                    {
+                        toProcess.offer( getLoc(newRow,newCol));
+                        visited.add(getPoint(newRow,newCol));
+                    }
+                }
+            }
+            length++;
+        }
+
+        return -1;
+        
+    }
+}
