657c88b09438c35dfdf006f8b09df5863a989495
Vipin Bhaskaran <mbvipin@gmail.com>
2026-07-07 07:40:17 +0100

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int [] row= new int [n];
+        Arrays.fill(row,1);
+
+        for(int i= m-2 ; i>= 0; i--)
+        {
+            int [] newRow= new int [n];
+            Arrays.fill(newRow,1);
+
+            for( int j = n-2; j>=0; j--)
+            {
+                newRow[j]= newRow[j+1]+ row[j];
+            }
+
+            row= newRow;
+
+
+        }
+
+        return row[0];
+        
+    }
+}
