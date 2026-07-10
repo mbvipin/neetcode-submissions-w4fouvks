c13e385f2c943487231af8a73e5c63db50b42a4c
Vipin Bhaskaran <mbvipin@gmail.com>
2026-07-10 08:02:17 +0100

    Add: unique-paths-ii - submission-6

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-6.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+       int M= obstacleGrid.length;
+       int N= obstacleGrid[0].length;
+
+       int [] dp= new int [N+1];
+
+        dp[N-1]=1;
+
+        for( int r= M-1; r >=0; r--)
+        {
+            for( int c= N-1; c>=0; c--)
+            {
+                if(obstacleGrid[r][c] ==1)
+                {
+                    dp[c]=0;
+                }
+
+                else
+                {
+                    dp[c] += dp[c+1];
+                }
+
+
+            }
+
+
+        }
+
+        return dp[0];
+
+        
+    }
+}
