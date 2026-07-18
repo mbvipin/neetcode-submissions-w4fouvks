a15734ebc7c3656dae31e9e381f595af52bc6b42
Vipin Bhaskaran <mbvipin@gmail.com>
2026-07-18 13:14:51 +0100

    Add: maximum-sum-circular-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+
+        int n= nums.length;
+         int [] rightMax= new int [n];
+        int suffixSum= nums[n-1];
+        rightMax[n-1]= nums[n-1];
+
+        for( int i= n-2 ; i >=0; i--)
+        {
+            suffixSum += nums[i];
+
+            rightMax[i]= Math.max(suffixSum, rightMax[i+1]);
+        }
+
+        int maxSum=nums[0];
+        int currSum=0;
+        int prefixSum=0;
+
+        for( int i=0; i < n; i++)
+        {
+            currSum= Math.max( currSum,0)+ nums[i];
+            maxSum= Math.max( maxSum, currSum);
+
+            prefixSum= prefixSum+ nums[i];
+
+            if( i < n-1)
+            {
+                maxSum= Math.max( maxSum, prefixSum+ rightMax[i+1]);
+            }
+        }
+
+        return maxSum;
+        
+    }
+}
