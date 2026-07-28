8cef8d35b63d08aeee57f06b5caaaed902cf558b
Vipin Bhaskaran <mbvipin@gmail.com>
2026-07-28 07:36:02 +0100

    Add: minimum-size-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-3.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+
+        int n= nums.length;
+
+        int [] prefixSum = new int [n+1];
+
+        for( int i=0; i< n ; i++)
+        {
+            prefixSum[i+1]= prefixSum[i]+ nums[i];
+        }
+
+        int res= n +1;
+
+        for( int i=0; i < n; i ++)
+        {
+            int l= i; 
+            int r= n;
+            while( l < r)
+            {
+                int mid= (l+ r)/2;
+
+                int currSum= prefixSum[mid +1]- prefixSum[i];
+
+                if(currSum >= target)
+                {
+                     r= mid;
+                }
+                else
+                {
+                    l = mid+1;
+                }
+
+               
+
+
+
+
+            }
+
+            if( l != n)
+            {
+                res= Math.min( res, l - i+1);
+            }
+        }
+
+        return res % (n+1);
+        
+    }
+}
