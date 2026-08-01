abe80d54009e3b5593b41f710c7e217ab167dad1
Vipin Bhaskaran <mbvipin@gmail.com>
2026-08-01 08:58:17 +0100

    Add: longest-turbulent-subarray - submission-0

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+
+        int n= arr.length;
+        int res=1;
+
+     for( int i=0; i < n-1; i++)
+     {
+        if( arr[i] == arr[i+1]) continue;
+
+        int sign= arr[i] > arr[i+1]? 1 :0;
+
+        int j= i+1;
+
+        while( j < n-1)
+        {
+            if( arr[j]== arr[j+1]) 
+            {
+                break;
+            }
+
+            int currSign= arr[j] > arr[j+1]? 1:0;
+
+            if( sign == currSign)
+            {
+                break;
+            }
+
+            sign= currSign;
+
+            j++;
+
+
+        }
+     
+        res= Math.max(res, j-i+1);
+
+     }
+
+     return res;
+        
+    }
+}
