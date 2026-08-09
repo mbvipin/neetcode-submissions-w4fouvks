d1dba5a86926f8afd7381f306caeb222e0569590
Vipin Bhaskaran <mbvipin@gmail.com>
2026-08-09 13:41:50 +0100

    Add: remove-duplicates-from-sorted-array-ii - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        Map<Integer,Integer> count= new HashMap<>();
+
+        List<Integer> arr= new ArrayList<>();
+
+        for(int num: nums)
+        {
+            count.put(num, count.getOrDefault(num,0)+1);
+
+            if(count.get(num) == 1)
+            {
+                arr.add(num);
+            }
+        }
+
+            int i=0;
+
+            for(int num: arr)
+            {
+                nums[i++]= num;
+                count.put(num, count.get(num)-1);
+
+                if( count.get(num) >=1 )
+                {
+                    nums[i++]= num;
+                    count.put(num, count.get(num)-1);
+                }
+
+
+            }
+
+    return i;
+
+        }
+
+    
+        
+    }
+    
