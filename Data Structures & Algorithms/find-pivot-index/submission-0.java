71485faecb994d02b07af58142fa62cabfa5f25f
Vipin Bhaskaran <mbvipin@gmail.com>
2026-08-23 12:38:37 +0100

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int n= nums.length;
+
+        for(int i=0; i < n; i++)
+        {
+            int leftSum=0;
+
+            for(int j=0; j <i; j++)
+            {
+                leftSum += nums[j];
+            }
+
+            int rightSum=0;
+
+            for(int k= i+1; k < n; k++)
+            {
+                rightSum += nums[k];
+
+            }
+
+            if( leftSum == rightSum)
+            {
+                return i;
+            }
+
+
+
+
+        }
+
+        return -1;
+        
+    }
+}
