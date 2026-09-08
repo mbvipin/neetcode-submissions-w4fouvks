096ce765707615c916e5c3d2c72b636d9fb0d6c6
Vipin Bhaskaran <mbvipin@gmail.com>
2026-09-08 07:28:50 +0100

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,88 @@
+class TrieNode
+{
+    private TrieNode [] children = new TrieNode [26];
+    private boolean isEndofWord;
+}
+
+
+class WordDictionary {
+
+    private TrieNode root;
+
+    public WordDictionary() {
+
+        root= new TrieNode();
+
+    }
+
+    public void addWord(String word) {
+
+        TrieNode curr= root;
+
+        for( Character c: word.toCharArray())
+        {
+            int idx= c - 'a';
+
+             if( curr.children[idx] == null)
+             {
+                curr.children[idx]= new TrieNode();
+             }
+
+             curr= curr.children[idx];
+
+
+        }
+
+        curr.isEndofWord= true;
+
+    }
+
+    public boolean search(String word) {
+
+        return dfs(word,0,root);
+
+    }
+
+    private boolean dfs(String word, int index, TrieNode root)
+    {
+        TrieNode curr= root;
+
+        for(int i= index; i < word.length(); i++)
+        {
+            char c= word.charAt(i);
+
+            if( c == '.')
+            {
+                for(TrieNode child: curr.children)
+                {
+                    if( child != null && dfs(word,i+1,child))
+                    {
+                        return true;
+                    }
+
+
+                }
+
+                return false;
+            }
+
+            else
+            {
+                if( curr.children[c - 'a'] == null)
+                {
+                    return false;
+                }
+
+                curr= curr.children[c - 'a'];
+
+
+            }
+
+
+        }
+
+
+         return curr.isEndofWord;
+
+    }
+}
