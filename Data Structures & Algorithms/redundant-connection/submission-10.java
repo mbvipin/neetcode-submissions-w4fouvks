d19900bf12f69392df2fcccd75d12de721f5489e
Vipin Bhaskaran <mbvipin@gmail.com>
2026-09-20 10:26:38 +0100

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+
+        int n= edges.length;
+
+        int [] par = new int [n+1];
+        int [] rank= new int [n+1];
+
+        for(int i=1; i <= n; i++)
+        {
+            par[i]=i;
+            rank[i]=1;
+        }
+
+        for(int [] edge: edges)
+        {
+            int u= edge[0];
+            int v= edge[1];
+
+            if( ! union(u,v,par,rank))
+            {
+                return new int [] {u,v};
+            }
+        }
+
+        return new int[0];
+        
+    }
+
+    public int find(int n,int [] par)
+    {
+        int p= par[n];
+
+        if( p != par[p])
+        {
+            par[p]= par[ find(par[p],par)];
+        }
+        
+        return par[p];
+
+    }
+
+    public boolean union(int u, int v,int [] par,int [] rank)
+    {
+        int p1= find(u,par);
+        int p2= find(v,par);
+
+        if( p1 == p2)
+        {
+            return false;
+        }
+
+        if( rank[p1] > rank[p2])
+        {
+            par[p2]=p1;
+            rank[p1]++;
+        }
+        else
+        {
+            par[p1]=p2;
+            rank[p2]++;
+        }
+
+        return true;
+
+    }
+}
